6f2cca5038abe2b1ee5d19a7281fcb3d0b567456
Drew Fate-Bolognone <afbolognone@luminarycloud.com>
2024-11-07 10:51:05 -0800

    zsh: vi mode backspace matches vim

--- dot_config/zsh/plugins/keybinds.zsh
+++ dot_config/zsh/plugins/keybinds.zsh
@@ -6,3 +6,6 @@ bindkey "\e[B" history-beginning-search-forward
 bindkey -M vicmd 'k' history-beginning-search-backward
 bindkey -M vicmd 'j' history-beginning-search-forward
 
+bindkey -M viins "^H" backward-delete-char
+bindkey -M viins "^?" backward-delete-char
+
