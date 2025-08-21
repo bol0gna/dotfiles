cde7b59d39fa0ec79f2fe612553d695ba637e7e1
Drew Fate-Bolognone <afbolognone@luminarycloud.com>
2025-08-21 11:32:33 -0700

    zsh: use interactive menu, keybind update

--- dot_config/zsh/plugins/keybinds.zsh
+++ dot_config/zsh/plugins/keybinds.zsh
@@ -3,9 +3,18 @@
 bindkey "\e[A" history-beginning-search-backward
 bindkey "\e[B" history-beginning-search-forward
 
-bindkey -M vicmd 'k' history-beginning-search-backward
-bindkey -M vicmd 'j' history-beginning-search-forward
+bindkey -M vicmd "k" history-beginning-search-backward
+bindkey -M vicmd "j" history-beginning-search-forward
 
 bindkey -M viins "^H" backward-delete-char
 bindkey -M viins "^?" backward-delete-char
+
+bindkey -M vicmd "?" history-incremental-pattern-search-backward
+
+# keybinds for 'menu select' completion (load before compinit)
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
 
